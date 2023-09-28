f7f5c3005da0d75e1e1eb6637b027c43b5961aa3
João Paulo <jp20171617@gmail.com>
2023-09-28 13:42:02 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SmallCardComponent } from './Components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainTitleComponent } from './Components/main-title/main-title.component';
+import { BigCardComponent } from './Components/big-card/big-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SmallCardComponent,
+    HomeComponent,
+    MainTitleComponent,
+    BigCardComponent,
   ],
   imports: [
     BrowserModule,
